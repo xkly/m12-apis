e0bf2c464ddfbb243eab04abbc284c31dccfe6a2
xkly <kly22@uw.edu>
2017-04-27 18:39:22 -0700

    started exercise 2

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,24 +1,40 @@
 ### Exercise 2 ###
 
 # Load the httr and jsonlite libraries for accessing data
-
+library(httr)
+library(jsonlite)
 
 # Create a `base.uri` variable that holds the base uri. You wil then paste endpoints to this base.
-
+base.uri <- "https://api.spotify.com"
 
 
 ## As you may have noticed, it often takes multiple queries to retrieve the desired information.
 ## This is a perfect situation in which writing a function will allow you to better structure your
 ## code, and give a name to a repeated task!
 
+resource <- "/v1/search"
+uri = paste0(base.uri, resource)
+query.params <- list(q = "Beyonce", type = "artist")
+response <- GET(uri, query = query.params)
+# don't have access to Spotify API?
+
 
 # Define a function `TopTrackSearch` that takes in an artist name as an argument,
 # and returns the top 10 tracks (in the US) by that artist
+TopTrackSearch <- function(artist.name) {
+  
+  # find id first
+  resource <- "/v1/search"
+  full.uri <- paste0(base.uri, resource)
+  query.param <- list(q = artist.name, type = "artist")
+  request <- GET(full.uri, query = query.param)
+  artist.info<- fromJSON(content(request, "text"))
+  
+}
 
+testfunction <- TopTrackSearch("Beyonce")
 
-
-
-# What are the top 10 tracks by Beyonce?
+  # What are the top 10 tracks by Beyonce?
 
 
 # Use the `flatten` function to flatten the data.frame -- note what differs!
